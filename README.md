7c9be8721a7ffa5beddb4049cc3705cf2d74fe07
Yasin Zabun <yasin.zabun@detaysoft.com>
2022-12-05 15:27:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# RedisSimpleClientExample
+Single Redis .Net7 client is caches to Redis a message then reads the message from Redis. All this happens in Docker.
+# Build Redis and .Net client
+cd /src
+docker-compose up -d
---
+++ src/Redis/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+Thread.Sleep(10000);
+Console.WriteLine("Console_App started!");
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect
+    (
+           new ConfigurationOptions
+           {
+               Password = Environment.GetEnvironmentVariable("RedisPassword"),
+               EndPoints = { Environment.GetEnvironmentVariable("ConnectionString") }
+           }
+    );
+
+var db = redis.GetDatabase();
+db.StringSetAsync("foo", "Hi, i'm foo").Wait();
+var foo = db.StringGetAsync("foo").Result;
+Console.WriteLine(foo.ToString());
+Console.WriteLine("Console_App finished!");
